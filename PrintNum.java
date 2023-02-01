b16c09eea71c5b90528c4829352ed2308f945730
Ayush Yadav <ayushcs.net24@gmail.com>
2023-02-01 22:36:44 +0530

    ..

---
+++ PrintNum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class PrintNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num=sc.nextInt();
+        int i=1;
+        while(i<=num)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
