20593dd6f766f1779f7feed9d0c018db4b729401
Ayush Yadav <ayushcs.net24@gmail.com>
2023-02-01 22:36:21 +0530

    .

---
+++ NumSum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class NumSum {
+    
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num=sc.nextInt();
+        int i=1;
+        int sum=0;
+        while(i<=num)
+        {
+            sum=sum+i;
+            i++;
+            
+        }
+        System.out.println(sum);
+    
+    
+}
+
+    
+}
