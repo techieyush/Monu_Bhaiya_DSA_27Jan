f39e88ce709814837d0b63df17cee2251d0402c4
Ayush Yadav <ayushcs.net24@gmail.com>
2023-02-02 22:58:31 +0530

    ..

---
+++ SimpleInterest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p=sc.nextInt();
+        int r=sc.nextInt();
+        int t=sc.nextInt();
+        int SI=(p*r*t)/100;
+        System.out.println(SI);
+    }
+    
+}
