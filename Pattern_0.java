678b3f090f4a91b410dfd69ca0ff7360af2d72d4
Ayush Yadav <ayushcs.net24@gmail.com>
2023-02-10 22:10:53 +0530

    .

---
+++ Pattern_0.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Pattern_0 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num=sc.nextInt();
+        int row=1;
+        int nst=num;
+        while(row<=num)
+        {
+            int i=1;
+            while(i<=nst)
+            {
+                System.out.print("* ");
+                i++;
+            }
+            row++;
+            System.out.println("");
+           
+        }
+    }
+    
+}
