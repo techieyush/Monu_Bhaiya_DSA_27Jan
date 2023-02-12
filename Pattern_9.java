ff64ff814f11e287579c8d9be63d0cefaef942bb
Ayush Yadav <ayushcs.net24@gmail.com>
2023-02-12 22:15:32 +0530

    .

---
+++ Pattern_9.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Pattern_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int num=sc.nextInt();
+        int row=1;
+        int nst=1;
+        int space=num-1;
+        while(row<=num)
+        {
+            int i=1;
+            while(i<=space)
+            {
+                System.out.print("  ");
+                i++;
+            }
+            int j=1;
+            while(j<=nst)
+            {
+                System.out.print("*");
+                j++;
+            }
+
+            System.out.println();
+            row++;
+            nst+=2;
+            space--;
+
+
+        }
+    }
+    
+}
