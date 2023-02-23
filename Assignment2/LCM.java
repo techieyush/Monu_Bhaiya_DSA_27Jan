1d275416aef4aa7fcbdb2a6ea325145079472e7a
Ayush Yadav <ayushcs.net24@gmail.com>
2023-02-23 20:16:06 +0530

    ..

--- Assignment2/LCM.java
+++ Assignment2/LCM.java
@@ -1,3 +1,25 @@
+import java.util.*;
 public class LCM {
+ 
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+            int divisor=sc.nextInt();
+            int divident=sc.nextInt();
+            int rem=0;
+            int a=divident;
+            int b=divisor;
+            while(divisor%divident!=0) 
+            {
+                rem=divident%divisor;
+                divident=divisor;
+                divisor=rem;
+            }
+           
     
-}
+         
+           int LCM=(a*b)/(divident);
+            System.out.println(LCM);
+    
+        }
+    }
+
--- Assignment2/PrintReverse.java
+++ Assignment2/PrintReverse.java
@@ -1,3 +1,21 @@
+import java.util.*;
 public class PrintReverse {
-    
+   
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num=sc.nextInt();
+        int rev=0;
+        while(num!=0)
+        {
+            int lastDigit=num%10;
+            System.out.print(lastDigit);
+            num=num/10;
+            
+        }
+        // Your Code Here
+    }
 }
+
+    
+
