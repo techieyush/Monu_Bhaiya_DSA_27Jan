de8c6a646eab8ab43e750b85db6d4690582d3896
Ayush Yadav <ayushcs.net24@gmail.com>
2023-03-03 23:33:53 +0530

    ..

---
+++ SelectionSort.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class SelectionSort {
+    
+
+
+
+
+
+    public static void selectionSort(int[] nums)
+    {
+        for(int i=0;i<nums.length;i++)
+        {
+            int min=i;//index-minimum value index
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[j]<nums[min])
+                {
+                    min=j;
+                }
+                
+                  
+                
+            }
+            int temp=nums[i];
+            nums[i]=nums[min];
+            nums[min]=temp;
+       }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size=sc.nextInt();
+        int nums[]=new int[size];
+        for(int i=0;i<nums.length;i++)
+        {
+            nums[i]=sc.nextInt();
+        }
+        selectionSort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            System.out.print(nums[i]+" ");
+        }
+        
+    }
+    
+}
+
+    
+
