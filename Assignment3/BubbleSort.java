0ad9a7b480b5bc4fc9d30b54cfa10b35f5b769fb
Ayush Yadav <ayushcs.net24@gmail.com>
2023-03-06 23:42:22 +0530

    ..

---
+++ Assignment3/BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class BubbleSort {
+    public static void bubbleSort(int[] nums)
+    {
+        for(int i=1;i<nums.length;i++)
+        {
+            for(int j=0;j<nums.length-i;j++)
+            {
+                if(nums[j]>nums[j+1])
+                {
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                
+                }
+            }
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int size=sc.nextInt();
+        int nums[]=new int[size];
+        for(int i=0;i<nums.length;i++)
+        {
+            nums[i]=sc.nextInt();
+        }
+        bubbleSort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            System.out.println(nums[i]+" ");
+        }
+    }
+    
+}
