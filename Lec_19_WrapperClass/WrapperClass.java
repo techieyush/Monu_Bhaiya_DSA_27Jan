6a791240b28df3ba14ebdf948981d595f3572cd3
Ayush Yadav <ayushcs.net24@gmail.com>
2023-03-27 22:18:47 +0530

    ..

---
+++ Lec_19_WrapperClass/WrapperClass.java
@@ -0,0 +1,27 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        int a=10;
+        Integer a1=20;
+        long l1=89;
+        Long l2=79l;
+        float f1=29.4f;
+        Float f2=39.9f;
+        Double d1=29.3;
+        double d2=189.09;
+
+        Integer c1=15;
+        Integer c2=15;
+        Integer c3=117;
+        Integer c5=117;
+        System.out.println(c1==c2);
+        System.out.println(c3==c5);
+
+        System.out.println(c1==c2);
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(a);
+        System.out.println(a1);
+    }
+    
+}
