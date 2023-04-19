15ae0588e934418c52f088a49de1c2ec6c39de7e
Ayush Yadav <ayushcs.net24@gmail.com>
2023-04-19 23:47:23 +0530

    //

--- Assignment3/HelpRamu.java
+++ Assignment3/HelpRamu.java
@@ -1,3 +1,48 @@
+import java.util.*;
+   
 public class HelpRamu {
     
-}
+    
+        
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+            int testCase=sc.nextInt();
+            while(testCase-->0)
+            {
+                int C1=sc.nextInt();
+                int C2=sc.nextInt();
+                int C3=sc.nextInt();
+                int C4=sc.nextInt();
+                int n=sc.nextInt();
+                int m=sc.nextInt();
+                int rickshaw[]=new int[n];
+                int cab[]=new int[m];
+                for(int i=0;i<rickshaw.length;i++)
+                {
+                    rickshaw[i]=sc.nextInt();
+                }
+                for(int i=0;i<cab.length;i++)
+                {
+                    cab[i]=sc.nextInt();
+                }
+    
+            int ric_ride=0;
+            int cab_ride=0;
+            for(int i=0;i<rickshaw.length;i++)
+            {
+                ric_ride+=Math.min(rickshaw[i]*C1,C2);
+            }
+             for(int i=0;i<cab.length;i++)
+            {
+                cab_ride+=Math.min(cab[i]*C1,C2);
+            }
+            ric_ride=Math.min(ric_ride,C3);
+            cab_ride=Math.min(cab_ride,C3);
+    
+           int ans=Math.min(ric_ride+cab_ride,C4);
+    
+            System.out.println(ans);
+            }
+        }
+    }
+
--- Assignment3/NextPermutation.java
+++ Assignment3/NextPermutation.java
@@ -1,6 +1,73 @@
+import java.util.*;
 public class NextPermutation {
-    public static void main(String[] args) {
-        
-    }
+   
+   
     
-}
+        public static void reverse(int nums[],int i,int j)
+        {
+            while(i<j)
+            {
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+                j--;
+            }
+        }
+    
+        public static void arrayPermutation(int nums[])
+        {
+            int p=0;
+            int q=0;
+            for(int i=nums.length-2;i>=0;i--)
+            {
+                if(nums[i+1]>nums[i])
+                {
+                    p=i;
+                    break;
+                }
+            }
+            for(int i=nums.length-1;i>p;i--)
+            {
+                if(nums[i]>nums[p])
+                {
+                    q=i;
+                    break;
+                }
+            }
+            if(p==0 && q==0)
+            {
+                reverse(nums,0,nums.length-1);
+                return ;
+            }
+    
+            int temp=nums[p];
+            nums[p]=nums[q];
+            nums[q]=temp;
+    
+            reverse(nums,p+1,nums.length-1);
+    
+        }
+    
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+            int testCase=sc.nextInt();
+            while(testCase-->0)
+            {
+                int n=sc.nextInt();
+                int nums[]=new int[n];
+                for(int i=0;i<n;i++)
+                {
+                    nums[i]=sc.nextInt();
+                }
+                arrayPermutation(nums);
+                 for(int i=0;i<n;i++)
+                {
+                   System.out.print(nums[i]+" ");
+                }
+                System.out.println();
+            }
+    
+        }
+    }
+
