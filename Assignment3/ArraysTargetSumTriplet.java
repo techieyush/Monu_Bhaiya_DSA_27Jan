4ccf9514dbb74e549f7c4a525baeae37f7574aaf
Ayush Yadav <ayushcs.net24@gmail.com>
2023-04-20 23:46:44 +0530

    //

--- Assignment3/ArraysTargetSumTriplet.java
+++ Assignment3/ArraysTargetSumTriplet.java
@@ -1,3 +1,57 @@
+import java.util.*;
 public class ArraysTargetSumTriplet {
     
+   
+
+
+
+    public static void sumTriplet(int [] nums,int target)
+    {
+        Arrays.sort(nums);
+        int i,j,k;
+       
+        for( i=0;i<nums.length-2;i++)
+        {
+           j=i+1;
+           k=nums.length-1;
+           while(j<k)
+            {
+               if(nums[i]+nums[j]+nums[k]==target)
+               {
+                   System.out.println(nums[i]+", "+nums[j]+" and "+nums[k]);
+                   j++;
+                   k--;
+               }
+               else if(nums[i]+nums[j]+nums[k]>target)
+               {
+                   k--;
+               }
+               else{
+                   j++;
+               }
+                        
+                        
+                     
+                    
+            }
+        }
+        
+
+      
+    }
+
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int size=sc.nextInt();
+        int nums[]=new int[size];
+        for(int i=0;i<nums.length;i++)
+        {
+            nums[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        sumTriplet(nums,target);
+
+    }
 }
+
--- Assignment3/CalculateTheSum.java
+++ Assignment3/CalculateTheSum.java
@@ -1,3 +1,37 @@
+import java.util.*;
 public class CalculateTheSum {
     
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner (System.in);
+        int n=sc.nextInt();
+        int nums[]=new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        int query=sc.nextInt();
+        while(query-->0)
+        {
+            int x=sc.nextInt();
+            int nums1[]=new int[n];
+            for(int i=0;i<nums.length;i++)
+            {
+                int j=i-x;
+                if(j<0)
+                {
+                    j=j+n;
+                }
+                nums1[i]=nums[i]+nums[j];
+            }
+            nums=nums1;
+        }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+        System.out.println(sum);
+    }
 }
+    
+
