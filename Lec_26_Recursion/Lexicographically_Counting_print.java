e1b94b14cd834f22fe6439565dbe6d2ecd3ff199
Ayush Yadav <ayushcs.net24@gmail.com>
2023-05-07 22:42:34 +0530

    //

--- Lec_26_Recursion/Lexicographically_Counting_print.java
+++ Lec_26_Recursion/Lexicographically_Counting_print.java
@@ -5,28 +5,54 @@ import java.util.List;
 
 public class Lexicographically_Counting_print {
 
-    public static void printCounting(int curr,int n,List<Integer>ll)
-    {
-        if(curr>n)
-        {
-            return;
-        }
-        // System.out.print(curr+"  ");
-        ll.add(curr);
-        int i=0;
-        if(curr==0)
-        {
-            i=1;
-        }
-        for(;i<=9;i++)
-        {
-            printCounting(curr*10+i, n,ll);
-        }
-    }
-    public static void main(String[] args) {
-        List<Integer>ll=new ArrayList<>();
-        int n=1000;
-        printCounting(0,n);
-    }
-    
+    // public static void printCounting(int curr,int n,List<Integer>ll)
+    // {
+    //     if(curr>n)
+    //     {
+    //         return;
+    //     }
+    //     // System.out.print(curr+"  ");
+    //     ll.add(curr);
+    //     int i=0;
+    //     if(curr==0)
+    //     {
+    //         i=1;
+    //     }
+    //     for(;i<=9;i++)
+    //     {
+    //         printCounting(curr*10+i, n,ll);
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     List<Integer>ll=new ArrayList<>();
+    //     int n=1000;
+    //     printCounting(0,n);
+    // }
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 1000;
+		printCounting(0, n);
+	}
+
+	public static void printCounting(int curr, int n) {
+		// TODO Auto-generated method stub
+		if (curr > n) {
+			return;
+		}
+		System.out.println(curr);
+		int i = 0;
+		if (curr == 0) {
+			i = 1;
+		}
+
+		for (; i <= 9; i++) {
+			  printCounting(curr*10+i, n);
+		}
+
+	}
+
 }
+    
+
