296a3c3dc8acb89c2040db6c6285a2fdae0cb180
Ayush Yadav <ayushcs.net24@gmail.com>
2023-05-19 23:34:48 +0530

    //

---
+++ Lec_35/Person.java
@@ -0,0 +1,31 @@
+package Lec_35;
+
+public class Person {
+    String name = "Anish";
+	int age = 22;
+
+	public void Intro_yourSelf() {
+
+		System.out.println("My Name is " + name + " and age is " + age);
+	}
+
+	public void Fun() {
+		System.out.println("Hey fun");
+	}
+
+	public Person() {
+
+	}
+
+	public Person(int age, String name) {
+		this.age = age;
+		this.name = name;
+
+	}
+
+	public static void main(String[] args) {
+		Person p = new Person(78, "Raj");
+		p.Intro_yourSelf();
+	}
+    
+}
---
+++ Lec_35/Person_Client.java
@@ -0,0 +1,21 @@
+package Lec_35;
+
+public class Person_Client {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("hey");
+		// Person p1 = new Person();
+		Person p = new Person(19, "Smridhi");
+		Person p1 = new Person(19, "Mohini");
+		p.Intro_yourSelf();
+		// constructor
+		Car c = new Car("Red", 801, 9);
+		//c.color="black";
+
+
+		p1.Fun();
+	}
+
+    
+}
