1067cb34d8dc2dbf93646a00c162fecc3de7cc2a
Ayush Yadav <ayushcs.net24@gmail.com>
2023-05-24 22:31:00 +0530

    //

---
+++ Lec_38/Dynamic_Stack.java
@@ -0,0 +1,16 @@
+package Lec_38;
+
+public class Dynamic_Stack extends Stack {
+	@Override
+	public void push(int item) throws Exception {
+		if (isfull()) {
+			int[] newarr = new int[2 * arr.length];
+			for (int i = 0; i < arr.length; i++) {
+				newarr[i] = arr[i];
+
+			}
+			arr = newarr;
+		}
+	super.push(item);
+
+	}
