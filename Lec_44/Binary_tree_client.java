7c19740fd1dd176ba6b1e574863ee643e48ca186
Ayush Yadav <ayushcs.net24@gmail.com>
2023-06-06 22:36:27 +0530

    //

---
+++ Lec_44/Binary_tree_client.java
@@ -0,0 +1,13 @@
+
+    public class Binary_tree_client {
+
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            BinaryTree bt = new BinaryTree();
+            bt.Display();
+    
+        }
+    
+    }
+    
+
