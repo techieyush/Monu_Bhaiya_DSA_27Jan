522c4b946cd1f2de118e0a617f6a873e1e4889b2
Ayush Yadav <ayushcs.net24@gmail.com>
2023-06-26 23:28:07 +0530

    OOPS-2

---
+++ Lec_49/DSAI.java
@@ -0,0 +1,8 @@
+package Lec_49;
+
+public interface DSAI extends StackI, Dynamic_Interface {
+    public void sayhey();
+
+	public void push(int item);
+    
+}
---
+++ Lec_49/Dynamic_Interface.java
@@ -0,0 +1,11 @@
+package Lec_49;
+
+public class Dynamic_Interface {
+    	public void push(int item);
+
+	public void Display();
+
+	public void fun();
+
+    
+}
