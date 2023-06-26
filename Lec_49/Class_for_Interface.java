52670aea05c032deb9e4f0b2be281366333e2240
Ayush Yadav <ayushcs.net24@gmail.com>
2023-06-26 23:27:57 +0530

    OOPS-2

---
+++ Lec_49/Class_for_Interface.java
@@ -0,0 +1,42 @@
+package Lec_49;
+
+public class Class_for_Interface {
+
+    @Override
+	public int pop() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int peek() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void Display() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void fun() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sayhey() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void push(int item) {
+		// TODO Auto-generated method stub
+
+	}
+
+    
+}
