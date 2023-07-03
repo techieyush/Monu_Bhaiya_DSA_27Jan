c1fce70513ccb52650ae3c94fa3b36dbadea18d0
Ayush Yadav <ayushcs.net24@gmail.com>
2023-07-03 23:45:36 +0530

    OOPS_2

---
+++ Lec_50/CarCompareterPrice.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+import Lec50.Cars;
+
+public class CarCompareterPrice implements Comparator<Cars> {
+
+	@Override
+	public int compare(Cars o1, Cars o2) {
+		// TODO Auto-generated method stub
+		return o2.price - o1.price;
+	}
+
+}
